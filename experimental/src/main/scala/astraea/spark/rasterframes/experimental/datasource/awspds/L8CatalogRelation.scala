s22s/pre-lt-raster-frames

Queryable AWS L8 catalog.

experimental/src/main/scala/astraea/spark/rasterframes/experimental/datasource/awsl8/L8CatalogRelation.scala
@@ -1,0 +1,61 @@
+/*
+ * This software is licensed under the Apache 2 license, quoted below.
+ *
+ * Copyright 2018 Astraea. Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *     [http://www.apache.org/licenses/LICENSE-2.0]
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *
+ */
+
+package astraea.spark.rasterframes.experimental.datasource.awsl8
+
+import geotrellis.util.LazyLogging
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.sources.{BaseRelation, TableScan}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Row, SQLContext}
+
+/**
+ *
+ * @author sfitch
+ * @since 9/28/17
+ */
+case class L8CatalogRelation(sqlContext: SQLContext, sceneListPath: String)
+  extends BaseRelation with TableScan with LazyLogging {
+
+  def schema = StructType(Seq(
+    StructField("productId", StringType, false),
+    StructField("entityId", StringType, false),
+    StructField("acquisitionDate", TimestampType, false),
+    StructField("cloudCover", FloatType, false),
+    StructField("processingLevel", StringType, false),
+    StructField("path", ShortType, false),
+    StructField("row", ShortType, false),
+    StructField("min_lat", DoubleType, false),
+    StructField("min_lon", DoubleType, false),
+    StructField("max_lat", DoubleType, false),
+    StructField("max_lon", DoubleType, false),
+    StructField("download_url", StringType, false)
+  ))
+
+  def buildScan(): RDD[Row] = {
+    val catalog = sqlContext.read
+      .schema(schema)
+      .option("header", "true")
+      .csv(sceneListPath)
+    catalog.rdd
+  }
+}
+
+

experimental/src/test/scala/astraea/spark/rasterframes/experimental/datasource/awsl8/L8CatalogRelationTest.scala
@@ -1,0 +1,43 @@
+/*
+ * This software is licensed under the Apache 2 license, quoted below.
+ *
+ * Copyright 2018 Astraea. Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *     [http://www.apache.org/licenses/LICENSE-2.0]
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *
+ */
+
+package astraea.spark.rasterframes.experimental.datasource.awsl8
+
+import astraea.spark.rasterframes.TestEnvironment
+import org.apache.spark.sql.functions._
+
+/**
+ *
+ * @author sfitch
+ * @since 1/5/18
+ */
+class L8CatalogRelationTest  extends TestEnvironment {
+  describe("Representing L8 scenes as a Spark data source") {
+    it("should provide a non-empty catalog") {
+      import spark.implicits._
+
+      val catalog = spark.read.format(L8CatalogDataSource.NAME).load()
+
+      val scenes = catalog.filter($"path" === 15 && $"row" === 34).orderBy(asc("cloudCover"))
+      scenes.show(false)
+      assert(scenes.count() > 1)
+    }
+  }
+}

locationtech/rasterframes

experimental/src/main/scala/astraea/spark/rasterframes/experimental/datasource/awspds/L8CatalogRelation.scala
@@ -1,0 +1,67 @@
+/*
+ * This software is licensed under the Apache 2 license, quoted below.
+ *
+ * Copyright 2018 Astraea. Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *     [http://www.apache.org/licenses/LICENSE-2.0]
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *
+ */
+
+package astraea.spark.rasterframes.experimental.datasource.awspds
+
+import geotrellis.util.LazyLogging
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.sources.{BaseRelation, TableScan}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{Row, SQLContext}
+
+/**
+ * Schema definition and parser for AWS PDS L8 scene data.
+ *
+ * @author sfitch
+ * @since 9/28/17
+ */
+case class L8CatalogRelation(sqlContext: SQLContext, sceneListPath: String)
+  extends BaseRelation with TableScan with LazyLogging {
+
+  def schema = StructType(Seq(
+    StructField("productId", StringType, false),
+    StructField("entityId", StringType, false),
+    StructField("acquisitionDate", TimestampType, false),
+    StructField("cloudCover", FloatType, false),
+    StructField("processingLevel", StringType, false),
+    StructField("path", ShortType, false),
+    StructField("row", ShortType, false),
+    StructField("min_lat", DoubleType, false),
+    StructField("min_lon", DoubleType, false),
+    StructField("max_lat", DoubleType, false),
+    StructField("max_lon", DoubleType, false),
+    StructField("download_url", StringType, false)
+  ))
+
+  def buildScan(): RDD[Row] = {
+    import sqlContext.implicits._
+    val catalog = sqlContext.read
+      .schema(schema)
+      .option("header", "true")
+      .csv(sceneListPath)
+      .withColumn("url",  regexp_replace($"download_url", "index.html", ""))
+      .drop("download_url")
+      .withColumnRenamed("url", "download_url")
+    catalog.rdd
+  }
+}
+
+

experimental/src/test/scala/astraea/spark/rasterframes/experimental/datasource/awspds/L8CatalogRelationTest.scala
@@ -1,0 +1,43 @@
+/*
+ * This software is licensed under the Apache 2 license, quoted below.
+ *
+ * Copyright 2018 Astraea. Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *     [http://www.apache.org/licenses/LICENSE-2.0]
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *
+ */
+
+package astraea.spark.rasterframes.experimental.datasource.awspds
+
+import astraea.spark.rasterframes.TestEnvironment
+import org.apache.spark.sql.functions._
+
+/**
+ *
+ * @author sfitch
+ * @since 1/5/18
+ */
+class L8CatalogRelationTest extends TestEnvironment {
+  describe("Representing L8 scenes as a Spark data source") {
+    it("should provide a non-empty catalog") {
+      import spark.implicits._
+
+      val catalog = spark.read.format(L8CatalogDataSource.NAME).load()
+
+      val scenes = catalog.filter($"path" === 15 && $"row" === 34).orderBy(asc("cloudCover"))
+      scenes.show(false)
+      assert(scenes.count() > 1)
+    }
+  }
+}