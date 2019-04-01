4110ae8b008f929f04b29bbfd5880c7ce48b5ab1
Phil Varner <pvarner@astrea.io>
2019-04-01 17:21:59 -0400

    CircleCI build configuration (+5 squashed commits) Squashed commits: [1dd3d111] remove circleci files [cc2a261e] circleci fixes [9b5bcaef] add dockerhub auth for circlci container [87161ea0] add s22s to circleci container name [c73780c5] add gdal to circleci container

--- core/src/test/scala/astraea/spark/rasterframes/ref/RasterSourceSpec.scala
+++ core/src/test/scala/astraea/spark/rasterframes/ref/RasterSourceSpec.scala
@@ -133,7 +133,7 @@ class RasterSourceSpec extends TestEnvironment with TestData {
   }
 
   describe("GDAL support") {
-    it("should read JPG2000 scene") {
+    it("should read JPEG2000 scene") {
       val src = RasterSource(localSentinel.toURI)
       src.readAll().flatMap(_.tile.statisticsDouble).size should be (64)
     }
