f2707c45e9fdabf358e5d72438c1bb7e83bfd96d
James McClain <jmcclain@azavea.com>
2019-05-02 11:01:10 -0400

    TEST

--- project/RFDependenciesPlugin.scala
+++ project/RFDependenciesPlugin.scala
@@ -59,6 +59,7 @@ object RFDependenciesPlugin extends AutoPlugin {
     // NB: Make sure to update the Spark version in pyrasterframes/python/setup.py
     rfSparkVersion := "2.3.2",
     rfGeoTrellisVersion := "2.2.0",
-    rfGeoMesaVersion := "2.2.1"
+    rfGeoMesaVersion := "2.2.1",
+    dependencyOverrides += "com.azavea.gdal" % "gdal-warp-bindings" % "33.bf51177"
   )
 }
