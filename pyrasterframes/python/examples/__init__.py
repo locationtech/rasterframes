af77f4140c1de0c250e7935c83bc29d0d5f4dd19
Jason T. Brown <jason@astraea.earth>
2019-05-07 09:30:30 -0400

    Use kryo sugar in python examples
    Signed-off-by: Jason T. Brown <jason@astraea.earth>

--- pyrasterframes/python/examples/__init__.py
+++ pyrasterframes/python/examples/__init__.py
@@ -1,4 +1,4 @@
-#examples_setup
+# examples_setup
 from pathlib import Path
 from pyspark.sql import SparkSession
 
@@ -7,19 +7,18 @@ __all__ = ["resource_dir", "example_session"]
 jarpath = list(Path('../target').resolve().glob('**/pyrasterframes*.jar'))
 if len(jarpath) > 0:
     pyJar = jarpath[0].as_uri()
+
     def example_session():
         return (SparkSession.builder
-        .master("local[*]")
-        .appName("RasterFrames")
-        .config('spark.driver.extraClassPath', pyJar)
-        .config('spark.executor.extraClassPath', pyJar)
-        .config("spark.ui.enabled", "false")
-        .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-        .config("spark.kryo.registrator", "org.locationtech.rasterframes.util.RFKryoRegistrator")
-        .config("spark.kryoserializer.buffer.max", "500m")
-        .getOrCreate())
+            .master("local[*]")
+            .appName("RasterFrames")
+            .config('spark.driver.extraClassPath', pyJar)
+            .config('spark.executor.extraClassPath', pyJar)
+            .config("spark.ui.enabled", "false")
+            .withKryoSerialization()
+            .getOrCreate())
 
 
 # hard-coded relative path for resources
 resource_dir = Path('./static').resolve()
-#examples_setup
+# examples_setup
