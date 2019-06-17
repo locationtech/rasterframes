3392a82632289d51a586973941102cb0ec4b0861
Simeon H.K. Fitch <fitch@astraea.io>
2019-06-17 19:37:32 -0400

    Moved Python tests back into main Python source directory (keep it pythonic).

--- pyrasterframes/src/main/python/tests/coverage-report.sh
+++ pyrasterframes/src/main/python/tests/coverage-report.sh
@@ -4,6 +4,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cd "$( dirname "${BASH_SOURCE[0]}" )"
+cd "$( dirname "${BASH_SOURCE[0]}" )"/..
 
 coverage run setup.py test && coverage html --omit='.eggs/*,setup.py' && open htmlcov/index.html
