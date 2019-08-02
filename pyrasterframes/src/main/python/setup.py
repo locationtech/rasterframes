76df01c921b5be2462ed5ea777f9179a4777e592
Jason T. Brown <jason@astraea.earth>
2019-08-02 12:06:33 -0400

    Pin python test to pyrsistent 0.15.3
    Signed-off-by: Jason T. Brown <jason@astraea.earth>

--- pyrasterframes/src/main/python/setup.py
+++ pyrasterframes/src/main/python/setup.py
@@ -128,7 +128,8 @@ setup(
         'setuptools>=0.8',
         'ipython==6.2.1',
         "ipykernel==4.8.0",
-        'Pweave==0.30.3'
+        'Pweave==0.30.3',
+        'pyrsistent==0.15.3',
     ],
     tests_require=[
         'pytest==3.4.2',
