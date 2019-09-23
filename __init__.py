ae98947a1f7a2da7b2bcc65a23ef6d7b448c0a4e
Simeon H.K. Fitch <fitch@astraea.io>
2019-09-23 11:22:12 -0400

    updated site

--- __init__.py
+++ __init__.py
@@ -18,26 +18,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-import os
 from pweave import PwebPandocFormatter
-
-
-def docs_dir():
-    return os.path.dirname(os.path.realpath(__file__))
-
-
-# This is temporary until we port to run on web assets.
-def resource_dir():
-    # we may consider using gitpython which I think would be appropriate in the context of building docs
-    # see https://stackoverflow.com/a/41920796/2787937
-    here = docs_dir()
-    test_resource = os.path.realpath(os.path.join(here, '..', '..', '..', 'src', 'test', 'resources'))
-
-    return test_resource
-
-
-def resource_dir_uri():
-    return 'file://' + resource_dir()
 
 
 class PegdownMarkdownFormatter(PwebPandocFormatter):
