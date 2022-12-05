ef2f4ee5d1cdc5599d115fc35ac338b800422528
Eugene Cheipesh <echeipesh@gmail.com>
2022-12-05 03:01:25 -0500

    Register functions directly
    this is a starting point

--- core/src/main/scala/org/locationtech/rasterframes/expressions/package.scala
+++ core/src/main/scala/org/locationtech/rasterframes/expressions/package.scala
@@ -22,13 +22,12 @@
 package org.locationtech.rasterframes
 
 import geotrellis.raster.{DoubleConstantNoDataCellType, Tile}
-import org.apache.spark.sql.catalyst.analysis.FunctionRegistry
+import org.apache.spark.sql.catalyst.analysis.{FunctionRegistryBase}
 import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
-import org.apache.spark.sql.catalyst.expressions.{Expression, ScalaUDF}
-import org.apache.spark.sql.catalyst.{InternalRow, ScalaReflection}
-import org.apache.spark.sql.rf.VersionShims._
+import org.apache.spark.sql.catalyst.expressions.{Expression, ExpressionDescription, ExpressionInfo, ScalaUDF}
+import org.apache.spark.sql.catalyst.{FunctionIdentifier, InternalRow, ScalaReflection}
 import org.apache.spark.sql.types.DataType
-import org.apache.spark.sql.{SQLContext, rf}
+import org.apache.spark.sql.{SQLContext}
 import org.locationtech.rasterframes.expressions.accessors._
 import org.locationtech.rasterframes.expressions.aggregates.CellCountAggregate.DataCells
 import org.locationtech.rasterframes.expressions.aggregates._
@@ -38,6 +37,7 @@ import org.locationtech.rasterframes.expressions.focalops._
 import org.locationtech.rasterframes.expressions.tilestats._
 import org.locationtech.rasterframes.expressions.transformers._
 
+import scala.reflect.ClassTag
 import scala.reflect.runtime.universe._
 
 /**
@@ -64,114 +64,166 @@ package object expressions {
   def udfiexpr[RT: TypeTag, A1: TypeTag](name: String, f: DataType => A1 => RT): Expression => ScalaUDF = (child: Expression) => {
     val ScalaReflection.Schema(dataType, _) = ScalaReflection.schemaFor[RT]
     ScalaUDF((row: A1) => f(child.dataType)(row), dataType, Seq(child), Seq(Option(ExpressionEncoder[RT]().resolveAndBind())), udfName = Some(name))
+
   }
 
-  def register(sqlContext: SQLContext): Unit = {
-    // Expression-oriented functions have a different registration scheme
-    // Currently have to register with the `builtin` registry due to Spark data hiding.
-    val registry: FunctionRegistry = rf.registry(sqlContext)
+  private def expressionInfo[T : ClassTag](name: String, since: Option[String], database: Option[String]): ExpressionInfo = {
+    val clazz = scala.reflect.classTag[T].runtimeClass
+    val df = clazz.getAnnotation(classOf[ExpressionDescription])
+    if (df != null) {
+      if (df.extended().isEmpty) {
+        new ExpressionInfo(
+          clazz.getCanonicalName,
+          database.orNull,
+          name,
+          df.usage(),
+          df.arguments(),
+          df.examples(),
+          df.note(),
+          df.group(),
+          since.getOrElse(df.since()),
+          df.deprecated(),
+          df.source())
+      } else {
+        // This exists for the backward compatibility with old `ExpressionDescription`s defining
+        // the extended description in `extended()`.
+        new ExpressionInfo(clazz.getCanonicalName, database.orNull, name, df.usage(), df.extended())
+      }
+    } else {
+      new ExpressionInfo(clazz.getCanonicalName, name)
+    }
+  }
 
-    registry.registerExpression[Add]("rf_local_add")
-    registry.registerExpression[Subtract]("rf_local_subtract")
-    registry.registerExpression[TileAssembler]("rf_assemble_tile")
-    registry.registerExpression[ExplodeTiles]("rf_explode_tiles")
-    registry.registerExpression[GetCellType]("rf_cell_type")
-    registry.registerExpression[SetCellType]("rf_convert_cell_type")
-    registry.registerExpression[InterpretAs]("rf_interpret_cell_type_as")
-    registry.registerExpression[SetNoDataValue]("rf_with_no_data")
-    registry.registerExpression[GetDimensions]("rf_dimensions")
-    registry.registerExpression[ExtentToGeometry]("st_geometry")
-    registry.registerExpression[GetGeometry]("rf_geometry")
-    registry.registerExpression[GeometryToExtent]("st_extent")
-    registry.registerExpression[GetExtent]("rf_extent")
-    registry.registerExpression[GetCRS]("rf_crs")
-    registry.registerExpression[RealizeTile]("rf_tile")
-    registry.registerExpression[CreateProjectedRaster]("rf_proj_raster")
-    registry.registerExpression[Multiply]("rf_local_multiply")
-    registry.registerExpression[Divide]("rf_local_divide")
-    registry.registerExpression[NormalizedDifference]("rf_normalized_difference")
-    registry.registerExpression[Less]("rf_local_less")
-    registry.registerExpression[Greater]("rf_local_greater")
-    registry.registerExpression[LessEqual]("rf_local_less_equal")
-    registry.registerExpression[GreaterEqual]("rf_local_greater_equal")
-    registry.registerExpression[Equal]("rf_local_equal")
-    registry.registerExpression[Unequal]("rf_local_unequal")
-    registry.registerExpression[IsIn]("rf_local_is_in")
-    registry.registerExpression[Undefined]("rf_local_no_data")
-    registry.registerExpression[Defined]("rf_local_data")
-    registry.registerExpression[Min]("rf_local_min")
-    registry.registerExpression[Max]("rf_local_max")
-    registry.registerExpression[Clamp]("rf_local_clamp")
-    registry.registerExpression[Where]("rf_where")
-    registry.registerExpression[Standardize]("rf_standardize")
-    registry.registerExpression[Rescale]("rf_rescale")
-    registry.registerExpression[Sum]("rf_tile_sum")
-    registry.registerExpression[Round]("rf_round")
-    registry.registerExpression[Abs]("rf_abs")
-    registry.registerExpression[Log]("rf_log")
-    registry.registerExpression[Log10]("rf_log10")
-    registry.registerExpression[Log2]("rf_log2")
-    registry.registerExpression[Log1p]("rf_log1p")
-    registry.registerExpression[Exp]("rf_exp")
-    registry.registerExpression[Exp10]("rf_exp10")
-    registry.registerExpression[Exp2]("rf_exp2")
-    registry.registerExpression[ExpM1]("rf_expm1")
-    registry.registerExpression[Sqrt]("rf_sqrt")
-    registry.registerExpression[Resample]("rf_resample")
-    registry.registerExpression[ResampleNearest]("rf_resample_nearest")
-    registry.registerExpression[TileToArrayDouble]("rf_tile_to_array_double")
-    registry.registerExpression[TileToArrayInt]("rf_tile_to_array_int")
-    registry.registerExpression[DataCells]("rf_data_cells")
-    registry.registerExpression[NoDataCells]("rf_no_data_cells")
-    registry.registerExpression[IsNoDataTile]("rf_is_no_data_tile")
-    registry.registerExpression[Exists]("rf_exists")
-    registry.registerExpression[ForAll]("rf_for_all")
-    registry.registerExpression[TileMin]("rf_tile_min")
-    registry.registerExpression[TileMax]("rf_tile_max")
-    registry.registerExpression[TileMean]("rf_tile_mean")
-    registry.registerExpression[TileStats]("rf_tile_stats")
-    registry.registerExpression[TileHistogram]("rf_tile_histogram")
-    registry.registerExpression[DataCells]("rf_agg_data_cells")
-    registry.registerExpression[CellCountAggregate.NoDataCells]("rf_agg_no_data_cells")
-    registry.registerExpression[CellStatsAggregate.CellStatsAggregateUDAF]("rf_agg_stats")
-    registry.registerExpression[HistogramAggregate.HistogramAggregateUDAF]("rf_agg_approx_histogram")
-    registry.registerExpression[LocalStatsAggregate.LocalStatsAggregateUDAF]("rf_agg_local_stats")
-    registry.registerExpression[LocalTileOpAggregate.LocalMinUDAF]("rf_agg_local_min")
-    registry.registerExpression[LocalTileOpAggregate.LocalMaxUDAF]("rf_agg_local_max")
-    registry.registerExpression[LocalCountAggregate.LocalDataCellsUDAF]("rf_agg_local_data_cells")
-    registry.registerExpression[LocalCountAggregate.LocalNoDataCellsUDAF]("rf_agg_local_no_data_cells")
-    registry.registerExpression[LocalMeanAggregate]("rf_agg_local_mean")
+  def register(sqlContext: SQLContext, database: Option[String] = None): Unit = {
+    val registry = sqlContext.sparkSession.sessionState.functionRegistry
 
-    registry.registerExpression[FocalMax](FocalMax.name)
-    registry.registerExpression[FocalMin](FocalMin.name)
-    registry.registerExpression[FocalMean](FocalMean.name)
-    registry.registerExpression[FocalMode](FocalMode.name)
-    registry.registerExpression[FocalMedian](FocalMedian.name)
-    registry.registerExpression[FocalMoransI](FocalMoransI.name)
-    registry.registerExpression[FocalStdDev](FocalStdDev.name)
-    registry.registerExpression[Convolve](Convolve.name)
+    def registerFunction[T <: Expression : ClassTag](name: String, since: Option[String] = None)(builder: Seq[Expression] => T): Unit = {
+      val id = FunctionIdentifier(name, database)
+      val info = FunctionRegistryBase.expressionInfo[T](name, since)
+      registry.registerFunction(id, info, builder)
+    }
 
-    registry.registerExpression[Slope](Slope.name)
-    registry.registerExpression[Aspect](Aspect.name)
-    registry.registerExpression[Hillshade](Hillshade.name)
+    def register1[T <: Expression : ClassTag](
+      name: String,
+      builder: Expression => T
+    ): Unit = registerFunction[T](name, None){ case Seq(a) => builder(a)
+    }
 
-    registry.registerExpression[Mask.MaskByDefined]("rf_mask")
-    registry.registerExpression[Mask.InverseMaskByDefined]("rf_inverse_mask")
-    registry.registerExpression[Mask.MaskByValue]("rf_mask_by_value")
-    registry.registerExpression[Mask.InverseMaskByValue]("rf_inverse_mask_by_value")
-    registry.registerExpression[Mask.MaskByValues]("rf_mask_by_values")
+    def register2[T <: Expression : ClassTag](
+      name: String,
+      builder: (Expression, Expression) => T
+    ): Unit = registerFunction[T](name, None){ case Seq(a, b) => builder(a, b) }
 
-    registry.registerExpression[DebugRender.RenderAscii]("rf_render_ascii")
-    registry.registerExpression[DebugRender.RenderMatrix]("rf_render_matrix")
-    registry.registerExpression[RenderPNG.RenderCompositePNG]("rf_render_png")
-    registry.registerExpression[RGBComposite]("rf_rgb_composite")
+    def register3[T <: Expression : ClassTag](
+      name: String,
+      builder: (Expression, Expression, Expression) => T
+    ): Unit = registerFunction[T](name, None){ case Seq(a, b, c) => builder(a, b, c) }
 
-    registry.registerExpression[XZ2Indexer]("rf_xz2_index")
-    registry.registerExpression[Z2Indexer]("rf_z2_index")
+    def register5[T <: Expression : ClassTag](
+      name: String,
+      builder: (Expression, Expression, Expression, Expression, Expression) => T
+    ): Unit = registerFunction[T](name, None){ case Seq(a, b, c, d, e) => builder(a, b, c, d, e) }
 
-    registry.registerExpression[transformers.ReprojectGeometry]("st_reproject")
+    register2("rf_local_add", Add(_, _))
+    register2("rf_local_subtract", Subtract(_, _))
+    registerFunction("rf_explode_tiles"){ExplodeTiles(1.0, None, _)}
+    register5("rf_assemble_tile", TileAssembler(_, _, _, _, _))
+    register1("rf_cell_type", GetCellType(_))
+    register2("rf_convert_cell_type", SetCellType(_, _))
+    register2("rf_interpret_cell_type_as", InterpretAs(_, _))
+    register2("rf_with_no_data", SetNoDataValue(_,_))
+    register1("rf_dimensions", GetDimensions(_))
+    register1("st_geometry", ExtentToGeometry(_))
+    register1("rf_geometry", GetGeometry(_))
+    register1("st_extent", GeometryToExtent(_))
+    register1("rf_extent", GetExtent(_))
+    register1("rf_crs", GetCRS(_))
+    register1("rf_tile", RealizeTile(_))
+    register3("rf_proj_raster", CreateProjectedRaster(_, _, _))
+    register2("rf_local_multiply", Multiply(_, _))
+    register2("rf_local_divide", Divide(_, _))
+    register2("rf_normalized_difference", NormalizedDifference(_,_))
+    register2("rf_local_less", Less(_, _))
+    register2("rf_local_greater", Greater(_, _))
+    register2("rf_local_less_equal", LessEqual(_, _))
+    register2("rf_local_greater_equal", GreaterEqual(_, _))
+    register2("rf_local_equal", Equal(_, _))
+    register2("rf_local_unequal", Unequal(_, _))
+    register2("rf_local_is_in", IsIn(_, _))
+    register1("rf_local_no_data", Undefined(_))
+    register1("rf_local_data", Defined(_))
+    register2("rf_local_min", Min(_, _))
+    register2("rf_local_max", Max(_, _))
+    register3("rf_local_clamp", Clamp(_, _, _))
+    register3("rf_where", Where(_, _, _))
+    register3("rf_standardize", Standardize(_, _, _))
+    register3("rf_rescale", Rescale(_, _ , _))
+    register1("rf_tile_sum", Sum(_))
+    register1("rf_round", Round(_))
+    register1("rf_abs", Abs(_))
+    register1("rf_log", Log(_))
+    register1("rf_log10", Log10(_))
+    register1("rf_log2", Log2(_))
+    register1("rf_log1p", Log1p(_))
+    register1("rf_exp", Exp(_))
+    register1("rf_exp10", Exp10(_))
+    register1("rf_exp2", Exp2(_))
+    register1("rf_expm1", ExpM1(_))
+    register1("rf_sqrt", Sqrt(_))
+    register3("rf_resample", Resample(_, _, _))
+    register2("rf_resample_nearest", ResampleNearest(_, _))
+    register1("rf_tile_to_array_double", TileToArrayDouble(_))
+    register1("rf_tile_to_array_int", TileToArrayInt(_))
+    register1("rf_data_cells", DataCells(_))
+    register1("rf_no_data_cells", NoDataCells(_))
+    register1("rf_is_no_data_tile", IsNoDataTile(_))
+    register1("rf_exists", Exists(_))
+    register1("rf_for_all", ForAll(_))
+    register1("rf_tile_min", TileMin(_))
+    register1("rf_tile_max", TileMax(_))
+    register1("rf_tile_mean", TileMean(_))
+    register1("rf_tile_stats", TileStats(_))
+    register1("rf_tile_histogram", TileHistogram(_))
+    register1("rf_agg_data_cells", DataCells(_))
+    register1("rf_agg_no_data_cells", CellCountAggregate.NoDataCells(_))
+    register1("rf_agg_stats", CellStatsAggregate.CellStatsAggregateUDAF(_))
+    register1("rf_agg_approx_histogram", HistogramAggregate.HistogramAggregateUDAF(_))
+    register1("rf_agg_local_stats", LocalStatsAggregate.LocalStatsAggregateUDAF(_))
+    register1("rf_agg_local_min",LocalTileOpAggregate.LocalMinUDAF(_))
+    register1("rf_agg_local_max", LocalTileOpAggregate.LocalMaxUDAF(_))
+    register1("rf_agg_local_data_cells", LocalCountAggregate.LocalDataCellsUDAF(_))
+    register1("rf_agg_local_no_data_cells", LocalCountAggregate.LocalNoDataCellsUDAF(_))
+    register1("rf_agg_local_mean", LocalMeanAggregate(_))
+    register3(FocalMax.name, FocalMax(_, _, _))
+    register3(FocalMin.name, FocalMin(_, _, _))
+    register3(FocalMean.name, FocalMean(_, _, _))
+    register3(FocalMode.name, FocalMode(_, _, _))
+    register3(FocalMedian.name, FocalMedian(_, _, _))
+    register3(FocalMoransI.name, FocalMoransI(_, _, _))
+    register3(FocalStdDev.name, FocalStdDev(_, _, _))
+    register3(Convolve.name, Convolve(_, _, _))
 
-    registry.registerExpression[ExtractBits]("rf_local_extract_bits")
-    registry.registerExpression[ExtractBits]("rf_local_extract_bit")
+    register3(Slope.name, Slope(_, _, _))
+    register2(Aspect.name, Aspect(_, _))
+    register5(Hillshade.name, Hillshade(_, _, _, _, _))
+
+    register2("rf_mask", Mask.MaskByDefined(_, _))
+    register2("rf_inverse_mask", Mask.InverseMaskByDefined(_, _))
+    register3("rf_mask_by_value", Mask.MaskByValue(_, _, _))
+    register3("rf_inverse_mask_by_value", Mask.InverseMaskByValue(_, _, _))
+    register2("rf_mask_by_values", Mask.MaskByValues(_, _))
+
+    register1("rf_render_ascii", DebugRender.RenderAscii(_))
+    register1("rf_render_matrix", DebugRender.RenderMatrix(_))
+    register1("rf_render_png", RenderPNG.RenderCompositePNG(_))
+    register3("rf_rgb_composite", RGBComposite(_, _, _))
+
+    register2("rf_xz2_index", XZ2Indexer(_, _, 18.toShort))
+    register2("rf_z2_index", Z2Indexer(_, _, 31.toShort))
+
+    register3("st_reproject", ReprojectGeometry(_, _, _))
+
+    register3[ExtractBits]("rf_local_extract_bits", ExtractBits(_: Expression, _: Expression, _: Expression))
+    register3[ExtractBits]("rf_local_extract_bit", ExtractBits(_: Expression, _: Expression, _: Expression))
   }
 }
