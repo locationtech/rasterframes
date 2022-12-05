8da8bd7ae056c5e574f31488f59f101a4a8d24a9
Eugene Cheipesh <echeipesh@gmail.com>
2022-12-05 03:00:39 -0500

    Bring in the Kryo setup
    GT settings bring in jackson classes and with shading it was getting weird

--- core/src/main/scala/org/locationtech/rasterframes/util/RFKryoRegistrator.scala
+++ core/src/main/scala/org/locationtech/rasterframes/util/RFKryoRegistrator.scala
@@ -21,11 +21,12 @@
 
 package org.locationtech.rasterframes.util
 
-import org.locationtech.rasterframes.ref.{DelegatingRasterSource, RasterRef, RFRasterSource}
+import org.locationtech.rasterframes.ref.{DelegatingRasterSource, RFRasterSource, RasterRef}
 import org.locationtech.rasterframes.ref._
 import com.esotericsoftware.kryo.Kryo
 import geotrellis.raster.io.geotiff.reader.GeoTiffInfo
-import geotrellis.spark.store.kryo.KryoRegistrator
+import geotrellis.spark.store.kryo.{GeometrySerializer}
+import org.apache.spark.serializer.KryoRegistrator
 
 /**
  *
@@ -35,7 +36,210 @@ import geotrellis.spark.store.kryo.KryoRegistrator
  */
 class RFKryoRegistrator extends KryoRegistrator {
   override def registerClasses(kryo: Kryo): Unit = {
-    super.registerClasses(kryo)
+
+    // TreeMap serializaiton has a bug; we fix it here as we're stuck on low
+    // Kryo versions due to Spark. Hack-tastic.
+    //kryo.register(classOf[util.TreeMap[_, _]], (new XTreeMapSerializer).asInstanceOf[com.esotericsoftware.kryo.Serializer[TreeMap[_, _]]])
+
+    kryo.register(classOf[(_,_)])
+    kryo.register(classOf[::[_]])
+    kryo.register(classOf[geotrellis.raster.ByteArrayFiller])
+
+    // CellTypes
+    kryo.register(geotrellis.raster.BitCellType.getClass)                     // Bit
+    kryo.register(geotrellis.raster.ByteCellType.getClass)                    // Byte
+    kryo.register(geotrellis.raster.ByteConstantNoDataCellType.getClass)
+    kryo.register(classOf[geotrellis.raster.ByteUserDefinedNoDataCellType])
+    kryo.register(geotrellis.raster.UByteCellType.getClass)                   // UByte
+    kryo.register(geotrellis.raster.UByteConstantNoDataCellType.getClass)
+    kryo.register(classOf[geotrellis.raster.UByteUserDefinedNoDataCellType])
+    kryo.register(geotrellis.raster.ShortCellType.getClass)                   // Short
+    kryo.register(geotrellis.raster.ShortConstantNoDataCellType.getClass)
+    kryo.register(classOf[geotrellis.raster.ShortUserDefinedNoDataCellType])
+    kryo.register(geotrellis.raster.UShortCellType.getClass)                  // UShort
+    kryo.register(geotrellis.raster.UShortConstantNoDataCellType.getClass)
+    kryo.register(classOf[geotrellis.raster.UShortUserDefinedNoDataCellType])
+    kryo.register(geotrellis.raster.IntCellType.getClass)                     // Int
+    kryo.register(geotrellis.raster.IntConstantNoDataCellType.getClass)
+    kryo.register(classOf[geotrellis.raster.IntUserDefinedNoDataCellType])
+    kryo.register(geotrellis.raster.FloatCellType.getClass)                   // Float
+    kryo.register(geotrellis.raster.FloatConstantNoDataCellType.getClass)
+    kryo.register(classOf[geotrellis.raster.FloatUserDefinedNoDataCellType])
+    kryo.register(geotrellis.raster.DoubleCellType.getClass)                  // Double
+    kryo.register(geotrellis.raster.DoubleConstantNoDataCellType.getClass)
+    kryo.register(classOf[geotrellis.raster.DoubleUserDefinedNoDataCellType])
+
+    // ArrayTiles
+    kryo.register(classOf[geotrellis.raster.BitArrayTile])                    // Bit
+    kryo.register(classOf[geotrellis.raster.ByteArrayTile])                   // Byte
+    kryo.register(classOf[geotrellis.raster.ByteRawArrayTile])
+    kryo.register(classOf[geotrellis.raster.ByteConstantNoDataArrayTile])
+    kryo.register(classOf[geotrellis.raster.ByteUserDefinedNoDataArrayTile])
+    kryo.register(classOf[geotrellis.raster.UByteArrayTile])                  // UByte
+    kryo.register(classOf[geotrellis.raster.UByteRawArrayTile])
+    kryo.register(classOf[geotrellis.raster.UByteConstantNoDataArrayTile])
+    kryo.register(classOf[geotrellis.raster.UByteUserDefinedNoDataArrayTile])
+    kryo.register(classOf[geotrellis.raster.ShortArrayTile])                  // Short
+    kryo.register(classOf[geotrellis.raster.ShortRawArrayTile])
+    kryo.register(classOf[geotrellis.raster.ShortConstantNoDataArrayTile])
+    kryo.register(classOf[geotrellis.raster.ShortUserDefinedNoDataArrayTile])
+    kryo.register(classOf[geotrellis.raster.UShortArrayTile])                 // UShort
+    kryo.register(classOf[geotrellis.raster.UShortRawArrayTile])
+    kryo.register(classOf[geotrellis.raster.UShortConstantNoDataArrayTile])
+    kryo.register(classOf[geotrellis.raster.UShortUserDefinedNoDataArrayTile])
+    kryo.register(classOf[geotrellis.raster.IntArrayTile])                    // Int
+    kryo.register(classOf[geotrellis.raster.IntRawArrayTile])
+    kryo.register(classOf[geotrellis.raster.IntConstantNoDataArrayTile])
+    kryo.register(classOf[geotrellis.raster.IntUserDefinedNoDataArrayTile])
+    kryo.register(classOf[geotrellis.raster.FloatArrayTile])                  // Float
+    kryo.register(classOf[geotrellis.raster.FloatRawArrayTile])
+    kryo.register(classOf[geotrellis.raster.FloatConstantNoDataArrayTile])
+    kryo.register(classOf[geotrellis.raster.FloatUserDefinedNoDataArrayTile])
+    kryo.register(classOf[geotrellis.raster.DoubleArrayTile])                 // Double
+    kryo.register(classOf[geotrellis.raster.DoubleRawArrayTile])
+    kryo.register(classOf[geotrellis.raster.DoubleConstantNoDataArrayTile])
+    kryo.register(classOf[geotrellis.raster.DoubleUserDefinedNoDataArrayTile])
+
+    kryo.register(classOf[Array[geotrellis.raster.Tile]])
+    kryo.register(classOf[Array[geotrellis.raster.TileFeature[_,_]]])
+    kryo.register(classOf[geotrellis.raster.Tile])
+    kryo.register(classOf[geotrellis.raster.TileFeature[_,_]])
+
+    kryo.register(classOf[geotrellis.raster.ArrayMultibandTile])
+    kryo.register(classOf[geotrellis.raster.CompositeTile])
+    kryo.register(classOf[geotrellis.raster.ConstantTile])
+    kryo.register(classOf[geotrellis.raster.CroppedTile])
+    kryo.register(classOf[geotrellis.raster.Raster[_]])
+    kryo.register(classOf[geotrellis.raster.RasterExtent])
+    kryo.register(classOf[geotrellis.raster.CellGrid[_]])
+    kryo.register(classOf[geotrellis.raster.CellSize])
+    kryo.register(classOf[geotrellis.raster.GridBounds[_]])
+    kryo.register(classOf[geotrellis.raster.GridExtent[_]])
+    kryo.register(classOf[geotrellis.raster.mapalgebra.focal.TargetCell])
+    kryo.register(classOf[geotrellis.raster.summary.GridVisitor[_, _]])
+    kryo.register(geotrellis.raster.mapalgebra.focal.TargetCell.All.getClass)
+    kryo.register(geotrellis.raster.mapalgebra.focal.TargetCell.Data.getClass)
+    kryo.register(geotrellis.raster.mapalgebra.focal.TargetCell.NoData.getClass)
+
+    kryo.register(classOf[geotrellis.layer.SpatialKey])
+    kryo.register(classOf[geotrellis.layer.SpaceTimeKey])
+    kryo.register(classOf[geotrellis.store.index.rowmajor.RowMajorSpatialKeyIndex])
+    kryo.register(classOf[geotrellis.store.index.zcurve.ZSpatialKeyIndex])
+    kryo.register(classOf[geotrellis.store.index.zcurve.ZSpaceTimeKeyIndex])
+    kryo.register(classOf[geotrellis.store.index.hilbert.HilbertSpatialKeyIndex])
+    kryo.register(classOf[geotrellis.store.index.hilbert.HilbertSpaceTimeKeyIndex])
+    kryo.register(classOf[geotrellis.vector.ProjectedExtent])
+    kryo.register(classOf[geotrellis.vector.Extent])
+    kryo.register(classOf[geotrellis.proj4.CRS])
+
+    // UnmodifiableCollectionsSerializer.registerSerializers(kryo)
+    kryo.register(geotrellis.raster.buffer.Direction.Center.getClass)
+    kryo.register(geotrellis.raster.buffer.Direction.Top.getClass)
+    kryo.register(geotrellis.raster.buffer.Direction.Bottom.getClass)
+    kryo.register(geotrellis.raster.buffer.Direction.Left.getClass)
+    kryo.register(geotrellis.raster.buffer.Direction.Right.getClass)
+    kryo.register(geotrellis.raster.buffer.Direction.TopLeft.getClass)
+    kryo.register(geotrellis.raster.buffer.Direction.TopRight.getClass)
+    kryo.register(geotrellis.raster.buffer.Direction.BottomLeft.getClass)
+    kryo.register(geotrellis.raster.buffer.Direction.BottomRight.getClass)
+
+    /* Exhaustive Registration */
+    kryo.register(classOf[Array[Double]])
+    kryo.register(classOf[Array[Float]])
+    kryo.register(classOf[Array[Int]])
+    kryo.register(classOf[Array[String]])
+    kryo.register(classOf[Array[org.locationtech.jts.geom.Coordinate]])
+    kryo.register(classOf[Array[org.locationtech.jts.geom.LinearRing]])
+    kryo.register(classOf[Array[org.locationtech.jts.geom.Polygon]])
+    kryo.register(classOf[Array[geotrellis.store.avro.AvroRecordCodec[Any]]])
+    kryo.register(classOf[Array[geotrellis.layer.SpaceTimeKey]])
+    kryo.register(classOf[Array[geotrellis.layer.SpatialKey]])
+    kryo.register(classOf[Array[geotrellis.vector.Feature[_, Any]]])
+    kryo.register(classOf[Array[geotrellis.vector.MultiPolygon]])
+    kryo.register(classOf[Array[geotrellis.vector.Point]])
+    kryo.register(classOf[Array[geotrellis.vector.Polygon]])
+    kryo.register(classOf[Array[scala.collection.Seq[Any]]])
+    kryo.register(classOf[Array[(Any, Any)]])
+    kryo.register(classOf[Array[(Any, Any, Any)]])
+    kryo.register(classOf[org.locationtech.jts.geom.Coordinate])
+    kryo.register(classOf[org.locationtech.jts.geom.Envelope])
+    kryo.register(classOf[org.locationtech.jts.geom.GeometryFactory])
+    kryo.register(classOf[org.locationtech.jts.geom.impl.CoordinateArraySequence])
+    kryo.register(classOf[org.locationtech.jts.geom.impl.CoordinateArraySequenceFactory])
+    kryo.register(classOf[org.locationtech.jts.geom.LinearRing])
+    kryo.register(classOf[org.locationtech.jts.geom.MultiPolygon])
+    kryo.register(classOf[org.locationtech.jts.geom.Point])
+    kryo.register(classOf[org.locationtech.jts.geom.Polygon])
+    kryo.register(classOf[org.locationtech.jts.geom.PrecisionModel])
+    kryo.register(classOf[org.locationtech.jts.geom.PrecisionModel.Type])
+    kryo.register(classOf[geotrellis.raster.histogram.FastMapHistogram])
+    kryo.register(classOf[geotrellis.raster.histogram.Histogram[AnyVal]])
+    kryo.register(classOf[geotrellis.raster.histogram.MutableHistogram[AnyVal]])
+    kryo.register(classOf[geotrellis.raster.histogram.StreamingHistogram])
+    kryo.register(classOf[geotrellis.raster.histogram.StreamingHistogram.DeltaCompare])
+    kryo.register(classOf[geotrellis.raster.histogram.StreamingHistogram.Delta])
+    kryo.register(classOf[geotrellis.raster.histogram.StreamingHistogram.Bucket])
+    kryo.register(classOf[geotrellis.raster.density.KernelStamper])
+    kryo.register(classOf[geotrellis.raster.ProjectedRaster[_]])
+    kryo.register(classOf[geotrellis.raster.TileLayout])
+    kryo.register(classOf[geotrellis.layer.TemporalProjectedExtent])
+    kryo.register(classOf[geotrellis.raster.buffer.BufferSizes])
+    kryo.register(classOf[geotrellis.store.avro.AvroRecordCodec[Any]])
+    kryo.register(classOf[geotrellis.store.avro.AvroUnionCodec[Any]])
+    kryo.register(classOf[geotrellis.store.avro.codecs.KeyValueRecordCodec[Any, Any]])
+    kryo.register(classOf[geotrellis.store.avro.codecs.TupleCodec[Any, Any]])
+    kryo.register(classOf[geotrellis.layer.KeyBounds[Any]])
+    kryo.register(classOf[geotrellis.spark.knn.KNearestRDD.Ord[Any]])
+    kryo.register(classOf[geotrellis.vector.Feature[_, Any]])
+    kryo.register(classOf[geotrellis.vector.Geometry], new GeometrySerializer[geotrellis.vector.Geometry])
+    kryo.register(classOf[geotrellis.vector.GeometryCollection])
+    kryo.register(classOf[geotrellis.vector.LineString], new GeometrySerializer[geotrellis.vector.LineString])
+    kryo.register(classOf[geotrellis.vector.MultiLineString], new GeometrySerializer[geotrellis.vector.MultiLineString])
+    kryo.register(classOf[geotrellis.vector.MultiPoint], new GeometrySerializer[geotrellis.vector.MultiPoint])
+    kryo.register(classOf[geotrellis.vector.MultiPolygon], new GeometrySerializer[geotrellis.vector.MultiPolygon])
+    kryo.register(classOf[geotrellis.vector.Point])
+    kryo.register(classOf[geotrellis.vector.Polygon], new GeometrySerializer[geotrellis.vector.Polygon])
+    kryo.register(classOf[geotrellis.vector.SpatialIndex[Any]])
+    kryo.register(classOf[java.lang.Class[Any]])
+    kryo.register(classOf[java.util.TreeMap[Any, Any]])
+    kryo.register(classOf[java.util.HashMap[Any, Any]])
+    kryo.register(classOf[java.util.HashSet[Any]])
+    kryo.register(classOf[java.util.LinkedHashMap[Any, Any]])
+    kryo.register(classOf[java.util.LinkedHashSet[Any]])
+    kryo.register(classOf[org.apache.hadoop.io.BytesWritable])
+    kryo.register(classOf[org.apache.hadoop.io.BigIntWritable])
+    kryo.register(classOf[Array[org.apache.hadoop.io.BigIntWritable]])
+    kryo.register(classOf[Array[org.apache.hadoop.io.BytesWritable]])
+    kryo.register(classOf[org.locationtech.proj4j.CoordinateReferenceSystem])
+    kryo.register(classOf[org.locationtech.proj4j.datum.AxisOrder])
+    kryo.register(classOf[org.locationtech.proj4j.datum.AxisOrder.Axis])
+    kryo.register(classOf[org.locationtech.proj4j.datum.Datum])
+    kryo.register(classOf[org.locationtech.proj4j.datum.Ellipsoid])
+    kryo.register(classOf[org.locationtech.proj4j.datum.Grid])
+    kryo.register(classOf[org.locationtech.proj4j.datum.Grid.ConversionTable])
+    kryo.register(classOf[org.locationtech.proj4j.util.PolarCoordinate])
+    kryo.register(classOf[org.locationtech.proj4j.util.FloatPolarCoordinate])
+    kryo.register(classOf[org.locationtech.proj4j.util.IntPolarCoordinate])
+    kryo.register(classOf[Array[org.locationtech.proj4j.util.FloatPolarCoordinate]])
+    kryo.register(classOf[org.locationtech.proj4j.datum.PrimeMeridian])
+    kryo.register(classOf[org.locationtech.proj4j.proj.LambertConformalConicProjection])
+    kryo.register(classOf[org.locationtech.proj4j.proj.LongLatProjection])
+    kryo.register(classOf[org.locationtech.proj4j.proj.TransverseMercatorProjection])
+    kryo.register(classOf[org.locationtech.proj4j.proj.MercatorProjection])
+    kryo.register(classOf[org.locationtech.proj4j.units.DegreeUnit])
+    kryo.register(classOf[org.locationtech.proj4j.units.Unit])
+    kryo.register(classOf[scala.collection.mutable.WrappedArray.ofInt])
+    kryo.register(classOf[scala.collection.mutable.WrappedArray.ofRef[AnyRef]])
+    kryo.register(classOf[scala.collection.Seq[Any]])
+    kryo.register(classOf[(Any, Any, Any)])
+    kryo.register(geotrellis.proj4.LatLng.getClass)
+    kryo.register(geotrellis.layer.EmptyBounds.getClass)
+    kryo.register(scala.collection.immutable.Nil.getClass)
+    kryo.register(scala.math.Ordering.Double.getClass)
+    kryo.register(scala.math.Ordering.Float.getClass)
+    kryo.register(scala.math.Ordering.Int.getClass)
+    kryo.register(scala.math.Ordering.Long.getClass)
+    kryo.register(scala.None.getClass)
     kryo.register(classOf[RFRasterSource])
     kryo.register(classOf[RasterRef])
     kryo.register(classOf[DelegatingRasterSource])
