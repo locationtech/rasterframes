a3ac4cf614e3bb7c335983337d2632743036ded1
Eugene Cheipesh <echeipesh@gmail.com>
2022-12-10 09:46:26 -0500

    Fix Resample and ResampleNearest
    Also untangle the super weird inheritance relationship between the two

--- core/src/main/scala/org/locationtech/rasterframes/expressions/localops/Resample.scala
+++ core/src/main/scala/org/locationtech/rasterframes/expressions/localops/Resample.scala
@@ -22,95 +22,19 @@
 package org.locationtech.rasterframes.expressions.localops
 
 import geotrellis.raster.Tile
-import geotrellis.raster.resample._
-import geotrellis.raster.resample.{Max => RMax, Min => RMin, ResampleMethod => GTResampleMethod}
+import geotrellis.raster.resample.{Mode, NearestNeighbor, Sum, Max => RMax, Min => RMin, ResampleMethod => GTResampleMethod}
 import org.apache.spark.sql.Column
-import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.analysis.TypeCheckResult
 import org.apache.spark.sql.catalyst.analysis.TypeCheckResult.{TypeCheckFailure, TypeCheckSuccess}
 import org.apache.spark.sql.catalyst.expressions.codegen.CodegenFallback
-import org.apache.spark.sql.catalyst.expressions.{Expression, ExpressionDescription, Literal, TernaryExpression}
+import org.apache.spark.sql.catalyst.expressions.{Expression, ExpressionDescription, TernaryExpression}
 import org.apache.spark.sql.functions.lit
 import org.apache.spark.sql.types.{DataType, StringType}
 import org.apache.spark.unsafe.types.UTF8String
-import org.locationtech.rasterframes.util.ResampleMethod
-import org.locationtech.rasterframes.expressions.{RasterResult, fpTile, row}
 import org.locationtech.rasterframes.expressions.DynamicExtractors._
+import org.locationtech.rasterframes.expressions.{RasterResult, fpTile, row}
+import org.locationtech.rasterframes.util.ResampleMethod
 
-
-abstract class ResampleBase(left: Expression, right: Expression, method: Expression) extends TernaryExpression with RasterResult with CodegenFallback with Serializable {
-
-  override val nodeName: String = "rf_resample"
-  def first: Expression = left
-  def second: Expression = right
-  def third: Expression = method
-  def dataType: DataType = left.dataType
-
-  def targetFloatIfNeeded(t: Tile, method: GTResampleMethod): Tile =
-    method match {
-      case NearestNeighbor | Mode | RMax | RMin | Sum  => t
-      case _ => fpTile(t)
-    }
-
-   // These methods define the core algorithms to be used.
-  def op(left: Tile, right: Tile, method: GTResampleMethod): Tile =
-    op(left, right.cols, right.rows, method)
-
-  def op(left: Tile, right: Double, method: GTResampleMethod): Tile =
-    op(left, (left.cols * right).toInt, (left.rows * right).toInt, method)
-
-  def op(tile: Tile, newCols: Int, newRows: Int, method: GTResampleMethod): Tile =
-    targetFloatIfNeeded(tile, method).resample(newCols, newRows, method)
-
-  override def checkInputDataTypes(): TypeCheckResult = {
-    // copypasta from BinaryLocalRasterOp
-    if (!tileExtractor.isDefinedAt(left.dataType)) {
-      TypeCheckFailure(s"Input type '${left.dataType}' does not conform to a raster type.")
-    }
-    else if (!tileOrNumberExtractor.isDefinedAt(right.dataType)) {
-      TypeCheckFailure(s"Input type '${right.dataType}' does not conform to a compatible type.")
-    } else method.dataType match {
-      case StringType => TypeCheckSuccess
-      case _ => TypeCheckFailure(s"Cannot interpret value of type `${method.dataType.simpleString}` for resampling method; please provide a String method name.")
-    }
-  }
-
-  override def nullSafeEval(input1: Any, input2: Any, input3: Any): Any = {
-    // more copypasta from BinaryLocalRasterOp
-
-    val (leftTile, leftCtx) = tileExtractor(left.dataType)(row(input1))
-    val methodString = input3.asInstanceOf[UTF8String].toString
-
-    val resamplingMethod = methodString match {
-      case ResampleMethod(mm) => mm
-      case _ => throw new IllegalArgumentException("Unrecognized resampling method specified")
-    }
-
-    val result: Tile = tileOrNumberExtractor(right.dataType)(input2) match {
-      // in this case we expect the left and right contexts to vary. no warnings raised.
-      case TileArg(rightTile, _) => op(leftTile, rightTile, resamplingMethod)
-      case DoubleArg(d) => op(leftTile, d, resamplingMethod)
-      case IntegerArg(i) => op(leftTile, i.toDouble, resamplingMethod)
-    }
-
-    // reassemble the leftTile with its context. Note that this operation does not change Extent and CRS
-    toInternalRow(result, leftCtx)
-  }
-
-  override def eval(input: InternalRow): Any = {
-    if(input == null) null
-    else {
-      val l = left.eval(input)
-      val r = right.eval(input)
-      val m = method.eval(input)
-      if (m == null) null // no method, return null
-      else if (l == null) null  // no l tile, return null
-      else if (r == null) l // no target tile or factor, return l without changin it
-      else nullSafeEval(l, r, m)
-    }
-  }
-
-}
 
 @ExpressionDescription(
   usage = "_FUNC_(tile, factor, method_name) - Resample tile to different dimension based on scalar `factor` or a tile whose dimension to match. Scalar less than one will downsample tile; greater than one will upsample. Uses resampling method named in the `method_name`." +
@@ -129,45 +53,74 @@ Examples:
   > SELECT _FUNC_(tile1, tile2, lit("cubic_spline"));
      ..."""
 )
-case class Resample(left: Expression, factor: Expression, method: Expression) extends ResampleBase(left, factor, method) {
+case class Resample(tile: Expression, factor: Expression, method: Expression) extends TernaryExpression with RasterResult with CodegenFallback {
+  override val nodeName: String = "rf_resample"
+  def dataType: DataType = tile.dataType
+  def first: Expression = tile
+  def second: Expression = factor
+  def third: Expression = method
+
+  override def checkInputDataTypes(): TypeCheckResult = {
+    if (!tileExtractor.isDefinedAt(tile.dataType)) {
+      TypeCheckFailure(s"Input type '${tile.dataType}' does not conform to a raster type.")
+    } else if (!tileOrNumberExtractor.isDefinedAt(factor.dataType)) {
+      TypeCheckFailure(s"Input type '${factor.dataType}' does not conform to a compatible type.")
+    } else
+      method.dataType match {
+        case StringType => TypeCheckSuccess
+        case _ =>
+          TypeCheckFailure(
+            s"Cannot interpret value of type `${method.dataType.simpleString}` for resampling method; please provide a String method name."
+          )
+      }
+  }
+  override def nullSafeEval(input1: Any, input2: Any, input3: Any): Any = {
+    val (leftTile, leftCtx) = tileExtractor(tile.dataType)(row(input1))
+    val ton = tileOrNumberExtractor(factor.dataType)(input2)
+    val methodString = input3.asInstanceOf[UTF8String].toString
+    val resamplingMethod = methodString match {
+      case ResampleMethod(mm) => mm
+      case _ => throw new IllegalArgumentException("Unrecognized resampling method specified")
+    }
+
+    val result: Tile = Resample.op(leftTile, ton, resamplingMethod)
+    toInternalRow(result, leftCtx)
+  }
+
   override protected def withNewChildrenInternal(newFirst: Expression, newSecond: Expression, newThird: Expression): Expression = copy(newFirst, newSecond, newThird)
 }
 
 object Resample {
-  def apply(left: Column, right: Column, methodName: String): Column =
-    new Column(Resample(left.expr, right.expr, lit(methodName).expr))
+  def op(tile: Tile, target: TileOrNumberArg, method: GTResampleMethod): Tile = {
+    val sourceTile = method match {
+      case NearestNeighbor | Mode | RMax | RMin | Sum => tile
+      case _ => fpTile(tile)
+    }
+    target match {
+      case TileArg(targetTile, _) =>
+        sourceTile.resample(targetTile.cols, targetTile.rows, method)
+      case DoubleArg(d) =>
+        sourceTile.resample((tile.cols * d).toInt, (tile.rows * d).toInt, method)
+      case IntegerArg(i) =>
+        sourceTile.resample(tile.cols * i,tile.rows * i, method)
+    }
+  }
 
-  def apply(left: Column, right: Column, method: Column): Column =
-    new Column(Resample(left.expr, right.expr, method.expr))
+  def apply(tile: Column, factor: Column, methodName: String): Column =
+    new Column(Resample(tile.expr, factor.expr, lit(methodName).expr))
 
-  def apply[N: Numeric](left: Column, right: N, method: String): Column = new Column(Resample(left.expr, lit(right).expr, lit(method).expr))
+  def apply(tile: Column, factor: Column, method: Column): Column =
+    new Column(Resample(tile.expr, factor.expr, method.expr))
 
-  def apply[N: Numeric](left: Column, right: N, method: Column): Column = new Column(Resample(left.expr, lit(right).expr, method.expr))
+  def apply[N: Numeric](tile: Column, factor: N, method: String): Column =
+    new Column(Resample(tile.expr, lit(factor).expr, lit(method).expr))
 
+  def apply[N: Numeric](tile: Column, factor: N, method: Column): Column =
+    new Column(Resample(tile.expr, lit(factor).expr, method.expr))
 }
 
-@ExpressionDescription(
- usage = "_FUNC_(tile, factor) - Resample tile to different size based on scalar factor or tile whose dimension to match. Scalar less than one will downsample tile; greater than one will upsample. Uses nearest-neighbor value.",
- arguments = """
-  Arguments:
-    * tile - tile
-    * rhs  - scalar or tile to match dimension""",
- examples = """
-  Examples:
-    > SELECT _FUNC_(tile, 2.0);
-       ...
-    > SELECT _FUNC_(tile1, tile2);
-       ...""")
-case class ResampleNearest(tile: Expression, target: Expression) extends ResampleBase(tile, target, Literal("nearest")) {
-  override val nodeName: String = "rf_resample_nearest"
 
-  override protected def withNewChildrenInternal(newFirst: Expression, newSecond: Expression, newThird: Expression): Expression =
-    ResampleNearest(tile, target)
-}
-object ResampleNearest {
-  def apply(tile: Column, target: Column): Column = new Column(ResampleNearest(tile.expr, target.expr))
 
-  def apply[N: Numeric](tile: Column, value: N): Column = new Column(ResampleNearest(tile.expr, lit(value).expr))
-}
+
 
 
