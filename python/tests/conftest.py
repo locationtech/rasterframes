071a21447219ff6b19a0af086d08aa1110747cd5
Thomas Maschler <thomas.maschler@cervest.earth>
2023-01-24 14:22:59 -0500

    fix import and assert brackets

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -116,10 +116,9 @@ def df(prdf):
 
 
 def assert_png(bytes):
-    assert (
-        bytes[0:8] == bytearray([0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]),
-        "png header does not match",
-    )
+    assert bytes[0:8] == bytearray(
+        [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]
+    ), "png header does not match"
 
 
 def rounded_compare(val1, val2):
